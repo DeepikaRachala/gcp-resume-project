2045580666e55e5d1ef1ba46729b76059753dd31
DeepikaRachala <deepikarachala92@gmail.com>
2024-08-09 18:14:03 +0530

    jquery error 2

--- resume-blog/static/js/visitor-count.js
+++ resume-blog/static/js/visitor-count.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    $.getJSON("https://us-central1-lucky-kayak-428217-m0.cloudfunctions.net/visitor-count-function",function(data){
-        $('#visitor-count').text(data.currentVisitor);
+jQuery(document).ready(function(){
+    jQuery.getJSON("https://us-central1-lucky-kayak-428217-m0.cloudfunctions.net/visitor-count-function",function(data){
+        jQuery('#visitor-count').text(data.currentVisitor);
 
     }).fail(function(){
         console.log("Error has occurred while retrieving the visitor-count data ")
