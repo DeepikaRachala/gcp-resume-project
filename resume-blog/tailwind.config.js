af8178f787d2af8105fc91102775016d365d90bd
DeepikaRachala <deepikarachala92@gmail.com>
2024-08-09 21:21:29 +0530

    media update

--- resume-blog/tailwind.config.js
+++ resume-blog/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./index.html","./static/js/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
   },
   plugins: [],
 }
